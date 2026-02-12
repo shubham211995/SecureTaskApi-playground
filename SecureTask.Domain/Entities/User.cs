75c848d526d77328ef5f3d3560ad9d27c2bf95b4
shubham211995 <shubhamtiwari211995@gmail.com>
2026-02-12 23:12:50 +0530

    Added fix forlogin api issue due to ID column

--- SecureTask.Domain/Entities/User.cs
+++ SecureTask.Domain/Entities/User.cs
@@ -1,5 +1,9 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 public class User
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
